1b727a5df768781b7a0cd3d0f3093eb678898a68
shenyuzhu1 <shenyuzhu@jd.com>
2019-06-15 11:20:36 +0800

    PropTypes

--- src/Todoitem.js
+++ src/Todoitem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // 父组件向子组件传参：使用参数
 // 子组件获取参数使用props
@@ -10,10 +11,28 @@ class Todoitem extends React.Component {
     deleteItem(index);
   }
   render() {
-    return (<div>
-      <li onClick={this.handleClick.bind(this)}>{this.props.content}</li>
-    </div>)
+    return (
+      <div>
+        {this.getContent()}
+      </div>
+    )
+  }
+
+  getContent() {
+    return (
+      <li onClick={this.handleClick.bind(this)}> {this.props.title} - {this.props.content}</li>
+    )
+
   }
 }
 
+Todoitem.propTypes = {
+  content: PropTypes.string,
+  index: PropTypes.number,
+  deleteItem: PropTypes.func
+}
+
+Todoitem.defaultProps = {
+  title: 'hello'
+}
 export default Todoitem;
